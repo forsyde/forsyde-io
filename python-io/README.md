84e929fac2afc1a1ee9ffae6aea6b3a4c3164cb6
jordao <jordao@kth.se>
2020-10-30 16:47:04 +0100

    Almost done for releasing

---
+++ python-io/README.md
@@ -0,0 +1,3 @@
+# ForSyDe IO Python library
+
+Supportin library for ForSyDe IO models in Python. Uses NetworkX for the core graph features.
--- python-io/setup.py
+++ python-io/setup.py
@@ -1,12 +1,23 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='forsyde-io-python',
-    version='0.5.1',
-    url='gitr',
-    author='Rodolfo Jordao',
-    author_email='jordao@kth.se',
-    description='Python bindings for reading, writing and manipulating ForSyDe-IO models',
-    packages=find_packages(),    
+    version='0.1.0',
+    author="Rodolfo Jordao",
+    author_email="jordao@kth.sw",
+    description="ForSyDe-IO Python supporting libraries",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Rojods/forsyde-io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     install_requires=['networkx'],
 )
