b44f0f5871f9f4af9c635d41fb070e472860442f
Rodolfo <jordao@kth.se>
2020-11-13 00:46:37 +0100

    Fixed SQL views

--- sql/create_sdf_views.sql
+++ sql/create_sdf_views.sql
@@ -1,5 +1,5 @@
-CREATE VIEW sdf_actors AS
-SELECT targets.vertex_id as 'actor_id',
+CREATE VIEW IF NOT EXISTS sdf_actors AS
+SELECT DISTINCT targets.vertex_id as 'actor_id',
        targets.type_name as 'actor_type',
        sources.vertex_id as 'constructor_id',
        sources.type_name as 'constructor_type'
@@ -13,8 +13,8 @@ WHERE
   targets.type_name = 'Process' AND
   sources.type_name = 'SDFComb';
 
-CREATE VIEW sdf_channels AS
-SELECT targets.vertex_id as 'channel_id',
+CREATE VIEW IF NOT EXISTS sdf_channels AS
+SELECT DISTINCT targets.vertex_id as 'channel_id',
        targets.type_name as 'channel_type'
   FROM vertexes AS targets
   JOIN edges ON targets.vertex_id = edges.target_vertex_id
@@ -23,15 +23,15 @@ WHERE
   (edges.type_name = 'Writes' OR edges.type_name = 'Reads') AND
   targets.type_name = 'Signal';
 
-CREATE VIEW sdf_topology_unsigned AS
-SELECT a.actor_id, c.channel_id, apropj.value, aprop.prop_id
-  FROM properties AS aprop
-  JOIN json_each(aprop.prop_value) AS apropj
-  JOIN sdf_actors AS a ON a.constructor_id = aprop.vertex_id
-  JOIN edges AS e ON a.actor_id = e.source_vertex_id
-  JOIN sdf_channels AS c ON c.channel_id = e.target_vertex_id;
+CREATE VIEW IF NOT EXISTS sdf_topology_unsigned AS
+SELECT DISTINCT a.actor_id, c.channel_id, apropj.value, aprop.prop_id
+  FROM properties AS aprop JOIN json_each(aprop.prop_value) AS apropj 
+  JOIN sdf_actors AS a ON a.constructor_id = aprop.vertex_id 
+  JOIN edges AS e ON a.actor_id = e.source_vertex_id 
+  JOIN sdf_channels AS c ON c.channel_id = e.target_vertex_id
+  WHERE e.source_Vertex_port_id = apropj.key;
 
-CREATE VIEW sdf_topology AS
+CREATE VIEW IF NOT EXISTS sdf_topology AS
 SELECT actor_id,
        channel_id,
        CASE WHEN prop_id = 'consumption' THEN -value
