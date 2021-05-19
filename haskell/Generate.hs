bf770333da33953ec80aaba1dde638fad531ddb6
Rodolfo Jordão <jordao@kth.se>
2021-05-19 22:13:09 +0200

    Can't make Haskell work

--- haskell/Generate.hs
+++ haskell/Generate.hs
@@ -1,10 +1,25 @@
 import Data.Aeson
 import Data.Aeson.Types
-import Data.Map
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as T
 
-parseVertexTraits :: Value -> Parser [String]
-parseVertexTraits (Object o) = return $ keys o
-parseVertexTraits _ = fail "expected vertex traits as object"
+parseVertexTraits :: Value -> Parser String
+parseVertexTraits v = (++) <$> firstTrait <*> restTraits
+  where
+    traitsStrings = map (\(k, v) -> k) . HM.toList <$> (parseJSON v :: Parser (HM.HashMap String Value))
+    top = pure "data VertexTrait\n" :: Parser String
+    firstTrait = (((++) "  = ") . head) <$> traitsStrings :: Parser String
+    restTraits = (concatMap (\v -> "  | " ++ v)) . (drop 1) <$> traitsStrings :: Parser String
 
 main :: IO ()
-main = putStrLn "hi"
+main = do
+  metaModelStr <- readFile "meta.json"
+  metaModel <- decode <$> (T.encodeUtf8 . TL.pack) metaModelStr
+  --   putStr show parseJSON (metaModel .: "vertexTraits")
+  writeFile "src/ForSyDe/IO/Haskell/Types.hs" contentString
+  where
+    header = "module ForSyDe.IO.Haskell.Types (VertexTrait(..), EdgeTrait(..)) where\n\n"
+    vertexTraits = "data VertexTrait = VertexTrait\n\n"
+    edgeTraits = "data EdgeTrait = EdgeTrait\n\n"
+    contentString = header ++ vertexTraits ++ edgeTraits
