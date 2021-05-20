ca4166a828869f220f665f691bb8b9ff7a3400b3
Rodolfo Jordão <jordao@kth.se>
2021-05-20 21:21:55 +0200

    First compilable haskell

--- haskell/Generate.hs
+++ haskell/Generate.hs
@@ -1,29 +1,39 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import System.IO
-import qualified Data.Text as T
-import Data.Maybe (fromJust)
-import Data.Aeson ( decode, FromJSON(parseJSON), Object, Value )
+import Data.Aeson (FromJSON (parseJSON), Object, Value, decode)
 import Data.Aeson.Types
 import qualified Data.ByteString.Lazy as B
 import qualified Data.HashMap.Strict as HM
+import Data.Maybe (fromJust)
+import qualified Data.Text as T
+import System.IO
 
-parseVertexTraits :: Value -> Parser String
-parseVertexTraits (Object o) = return $ top ++ firstTrait ++ restTraits
+parseVertexTraits :: Object -> Parser String
+parseVertexTraits o = return $ top ++ firstTrait ++ restTraits ++ footer ++ "\n\n"
   where
     traitsStrings = map fst . HM.toList $ o
     top = "data VertexTrait\n" :: String
     firstTrait = "  = " ++ (T.unpack . head $ traitsStrings) ++ "\n"
-    restTraits = concatMap (\v -> "  | " ++ (T.unpack v) ++ "\n") . drop 1 $ traitsStrings :: String
-parseVertexTraits _ = fail "Only objects!"
+    restTraits = concatMap (\v -> "  | " ++ T.unpack v ++ "\n") . drop 1 $ traitsStrings :: String
+    footer = "  deriving (Show, Read, Eq)"
+
+parseEdgeTraits :: Object -> Parser String
+parseEdgeTraits o = return $ top ++ firstTrait ++ restTraits ++ footer ++ "\n\n"
+  where
+    traitsStrings = map fst . HM.toList $ o
+    top = "data EdgeTrait\n" :: String
+    firstTrait = "  = " ++ (T.unpack . head $ traitsStrings) ++ "\n"
+    restTraits = concatMap (\v -> "  | " ++ T.unpack v ++ "\n") . drop 1 $ traitsStrings :: String
+    footer = "  deriving (Show, Read, Eq)"
 
 main :: IO ()
 main = do
   metaModelStr <- B.readFile "meta.json"
-  -- let metaModel = fromJust $ decode metaModelStr :: Value
-  let vertexTraitsStr = fromJust $ parseMaybe parseVertexTraits $ "vertexTraits" .: metaModelStr
-  writeFile "src/ForSyDe/IO/Haskell/Types.hs" $ header ++ vertexTraitsStr ++ edgeTraits
-  where 
-    header = "module ForSyDe.IO.Haskell.Types (VertexTrait(..), EdgeTrait(..)) where\n\n"
-    edgeTraits = "data EdgeTrait = EdgeTrait\n\n" 
-    
+  let metaModel = fromJust $ decode metaModelStr :: Object
+  let vertexTraits = fromJust $ parseMaybe (.: "vertexTraits") metaModel
+  let vertexTraitsStr = fromJust $ parseMaybe parseVertexTraits vertexTraits
+  let edgeTraits = fromJust $ parseMaybe (.: "edgeTraits") metaModel
+  let edgeTraitsStr = fromJust $ parseMaybe parseEdgeTraits edgeTraits
+  writeFile "src/ForSyDe/IO/Haskell/Types.hs" $ header ++ vertexTraitsStr ++ edgeTraitsStr
+  where
+    header = "module ForSyDe.IO.Haskell.Types (VertexTrait (..), EdgeTrait (..)) where\n\n"
