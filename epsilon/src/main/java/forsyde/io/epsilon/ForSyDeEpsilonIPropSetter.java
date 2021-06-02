5b6c3a5f29900e0e657a59a71152391e98d0f99f
jordao <jordao@kth.se>
2021-06-02 22:17:15 +0200

    Added initial Prop setter

---
+++ epsilon/src/main/java/forsyde/io/epsilon/ForSyDeEpsilonIPropSetter.java
@@ -0,0 +1,64 @@
+package forsyde.io.epsilon;
+
+import org.eclipse.epsilon.common.module.ModuleElement;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.eol.execute.context.IEolContext;
+import org.eclipse.epsilon.eol.execute.introspection.IPropertySetter;
+
+public class ForSyDeEpsilonIPropSetter implements IPropertySetter {
+
+	@Override
+	public void invoke(Object value) throws EolRuntimeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setProperty(String property) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getProperty() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setObject(Object object) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object getObject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ModuleElement getAst() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setAst(ModuleElement ast) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setContext(IEolContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IEolContext getContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
