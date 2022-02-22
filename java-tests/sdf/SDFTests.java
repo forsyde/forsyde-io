8685a1ec59ee8f9757ec42c2c151794e10cef5c0
Rodolfo Jordão <jordao@kth.se>
2022-02-22 20:59:06 +0100

    Added contributed test and increased reliability.

--- java-tests/sdf/SDFTests.java
+++ java-tests/sdf/SDFTests.java
@@ -1,9 +1,6 @@
 package sdf;
 
-import forsyde.io.java.core.EdgeInfo;
-import forsyde.io.java.core.ForSyDeSystemGraph;
-import forsyde.io.java.core.Vertex;
-import forsyde.io.java.core.VertexViewer;
+import forsyde.io.java.core.*;
 import forsyde.io.java.drivers.ForSyDeModelHandler;
 import forsyde.io.java.typed.viewers.moc.sdf.SDFChannel;
 import forsyde.io.java.typed.viewers.moc.sdf.SDFComb;
@@ -39,6 +36,29 @@ public class SDFTests {
         }
         Assertions.assertEquals(4, actors.size());
         Assertions.assertEquals(4, channels.size());
+
+        // contributed from https://github.com/YihangZhao123/master-thesis
+        Assertions.assertTrue(actors.stream().anyMatch(v ->
+            VertexAcessor.getNamedPort(forSyDeSystemGraph, v.getViewedVertex(), "gy", "moc::sdf::SDFChannel").isPresent() &&
+            VertexAcessor.getNamedPort(forSyDeSystemGraph, v.getViewedVertex(), "gx", "moc::sdf::SDFChannel").isPresent()
+        ));
+        Assertions.assertTrue(actors.stream().anyMatch(v ->
+                VertexAcessor.getNamedPort(forSyDeSystemGraph, v.getViewedVertex(), "gy", "moc::sdf::SDFChannel").map(SDFChannel::conforms).orElse(false) &&
+                VertexAcessor.getNamedPort(forSyDeSystemGraph, v.getViewedVertex(), "gx", "moc::sdf::SDFChannel").map(SDFChannel::conforms).orElse(false)
+        ));
+
+        // check that sobel/getPx outputs 6
+        Assertions.assertTrue(forSyDeSystemGraph.edgeSet().stream().filter(e ->
+                    e.hasTrait(EdgeTrait.MOC_SDF_SDFDATAEDGE) &&
+                    forSyDeSystemGraph.getEdgeSource(e).getIdentifier().equals("sobel/getPx") &&
+                    SDFComb.conforms(forSyDeSystemGraph.getEdgeSource(e)) &&
+                    SDFChannel.conforms(forSyDeSystemGraph.getEdgeSource(e))
+                ).allMatch(e ->
+                    SDFComb.safeCast(forSyDeSystemGraph.getEdgeSource(e)).flatMap(sourceSDF ->
+                        e.getSourcePort().map(port -> sourceSDF.getProduction().get(port).equals(6))
+                    ).orElse(false)
+                ));
+
     }
 
 }
