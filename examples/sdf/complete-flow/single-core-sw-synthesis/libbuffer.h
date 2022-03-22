c49826d4f8cd041da10953d4032611826b2a716d
Rodolfo Jordão <jordao@kth.se>
2022-03-22 23:09:07 +0100

    First possible SW synth target added.

--- examples/sdf/complete-flow/single-core-sw-synthesis/libbuffer.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIBBUFFER_
-#define LIBBUFFER_
-
-#define DECLARE_CIRCULAR_BUFFER(buffer_name, token_type, max_size) typedef struct {token_type[max_size]; size_t front; size_t rear; size_t max}
-
-
-
-#endif // LIBBUFFER_
