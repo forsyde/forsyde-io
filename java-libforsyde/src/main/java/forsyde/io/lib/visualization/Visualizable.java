e9b6af0a8694ac4e1e7390ddd2652230b537ee5b
Rodolfo Jordao <jordao@kth.se>
2023-06-02 21:59:40 +0200

    Python gitignore messing up

---
+++ java-libforsyde/src/main/java/forsyde/io/lib/visualization/Visualizable.java
@@ -0,0 +1,8 @@
+package forsyde.io.lib.visualization;
+
+import forsyde.io.java.core.VertexViewer;
+import forsyde.io.java.core.annotations.GenerateViewer;
+
+@GenerateViewer
+public interface Visualizable extends VertexViewer {
+}
